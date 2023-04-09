6b8d932c22e5c1da4024d5205e46f4d2c2aa93b5
Alex <sashavilkov2005@yandex.ru>
2023-04-09 12:30:50 +0300

    long polling working

--- Telegram/Telegram.py
+++ Telegram/Telegram.py
@@ -1,20 +1,25 @@
 import aiohttp
 from . import DataTypes
 import json
+from typing import List
 
 
 class Telegram():
     def __init__(self, token: str):
         self.session = aiohttp.ClientSession()
         self.apiEndpoint = f'https://api.telegram.org/bot{token}/'
+        self.offset = 0
         
-    async def getMe(self):
+    async def getMe(self) -> DataTypes.User:
         async with self.session.get(self.apiEndpoint + 'getMe') as response:
             if await json.loads(response.text())['ok']:
                 return DataTypes.User(json.loads(await response.text())['result'])
 
-    async def getUpdates(self):
-        async with self.session.get(self.apiEndpoint + 'getUpdates') as response:
+    async def getUpdates(self) -> List:
+        async with self.session.get(self.apiEndpoint + 'getUpdates', params={'timeout': 10, 'offset': self.offset}) as response:
             response = json.loads(await response.text())
-            if response['ok']:
+            if response['ok'] and response['result']:
+                self.offset = response['result'][-1]['update_id'] + 1
                 return [DataTypes.Update(i) for i in response['result']]
+            else:
+                return []
--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import asyncio
 
 async def main():
     bot = Telegram.Telegram('6029827241:AAHH3CtJ9WypzEBYn2-u73J096KEuyZ8SZU')
-    print(await bot.getUpdates())
+    for i in range(3):
+        print((await bot.getUpdates()))
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    print('start')
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
