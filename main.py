da11f91aac4a2064e473933c119e4c49dcfb9fae
Alex <sashavilkov2005@yandex.ru>
2023-04-14 06:43:02 +0300

    minor works

--- main.py
+++ main.py
@@ -6,27 +6,23 @@ import config
 from time import time
 
 
-async def echo(bot: Telegram.Telegram, update: Telegram.DataTypes.Update):
-    await bot.sendMessage(update.message.chat, update.message.text)
-
-
 async def kek(bot: Telegram.Telegram, update: Telegram.DataTypes.Update):
     message = await bot.sendMessage(update.message.chat, 'Бот обрабатывает ваш запрос...')
     comp = await openai.ChatCompletion.acreate(
         model='gpt-3.5-turbo',
         messages=[
-            {'role': 'user', 'content': 'расскажи максимально подробно про интегралы с примерами'}
+            {'role': 'user', 'content': f'{update.message.text}'}
         ],
         stream=True
     )
     j = ''
     t1, t2 = time(), time()
     async for i in comp:
-        await bot.sendChatAction(message.chat, Telegram.Actions.TYPING)
         j += i['choices'][0]['delta'].get('content', '')
         t1 = time()
         if t1 - t2 > 3 and j != message.text and j:
             t1, t2 = time(), t1
+            await bot.sendChatAction(message.chat, Telegram.Actions.TYPING)
             message = await bot.editMessageText(message.chat, message, j)
     await bot.editMessageText(message.chat, message, j)
 
@@ -36,9 +32,7 @@ if __name__ == '__main__':
     openai.api_key = config.OPENAI_API_KEY
     loop = asyncio.get_event_loop()
     bot = Telegram.Telegram(config.TELEGRAM_API_KEY)
-    echoHandler = Telegram.Handler('', echo)
-    kekHandler = Telegram.Handler('кек', kek)
+    kekHandler = Telegram.Handler('', kek)
     bot.addHandler(kekHandler)
-    bot.addHandler(echoHandler)
     loop.create_task(bot.run())
     loop.run_forever()
